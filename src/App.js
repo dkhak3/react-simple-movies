37c0443ad97426940549b9fe58c5ea43af2d153d
KHA  DUY <nhokkha345@gmail.com>
2023-05-02 15:34:17 +0700

    update

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import { NavLink } from "react-router-dom";
 function App() {
   return (
     <Fragment>
-      <header className="header flex items-center justify-center gap-x-5 text-white py-10 mb-10">
+      <header className="header flex items-center justify-center gap-x-5 text-white py-10 mb-5">
         <span className="text-primary">Home</span>
         <span>Movies</span>
       </header>
-      <section className="banner h-[400px] page-container">
+
+      <section className="banner h-[500px] page-container mb-20">
         <div className="w-full h-full rounded-lg relative">
           <div className="overlay absolute inset-0 bg-gradient-to-t from-[rgba(0,0,0,0.5)] to-[rgba(0,0,0,0.5)] rounded-lg"></div>
           <img
@@ -30,6 +31,75 @@ function App() {
               </span>
             </div>
             <button className="py-3 px-6 rounded-lg bg-primary text-white font-medium">
+              Watch now
+            </button>
+          </div>
+        </div>
+      </section>
+
+      <section className="movies-layout page-container pb-20">
+        <h2 className="capitalize text-white mb-10 text-3xl font-bold">
+          Now playing
+        </h2>
+        <div className="movie-list grid grid-cols-4 gap-10">
+          <div className="movie-card rounded-lg p-3 bg-slate-800 text-white">
+            <img
+              src="https://nld.mediacdn.vn/2019/4/25/3515432-endgamedek-15561710302491765206118.jpg"
+              alt=""
+              className="w-full h-[250px] object-cover rounded-lg mb-5"
+            />
+            <h3 className="text-xl font-bold mb-3">Onpiece</h3>
+            <div className="flex items-center justify-between text-sm opacity-50 mb-10">
+              <span>2023</span>
+              <span>9.0</span>
+            </div>
+            <button className="py-3 px-6 rounded-lg capitalize bg-primary w-full">
+              Watch now
+            </button>
+          </div>
+        </div>
+      </section>
+
+      <section className="movies-layout page-container pb-20">
+        <h2 className="capitalize text-white mb-10 text-3xl font-bold">
+          Top rated
+        </h2>
+        <div className="movie-list grid grid-cols-4 gap-10">
+          <div className="movie-card rounded-lg p-3 bg-slate-800 text-white">
+            <img
+              src="https://nld.mediacdn.vn/2019/4/25/3515432-endgamedek-15561710302491765206118.jpg"
+              alt=""
+              className="w-full h-[250px] object-cover rounded-lg mb-5"
+            />
+            <h3 className="text-xl font-bold mb-3">Onpiece</h3>
+            <div className="flex items-center justify-between text-sm opacity-50 mb-10">
+              <span>2023</span>
+              <span>9.0</span>
+            </div>
+            <button className="py-3 px-6 rounded-lg capitalize bg-primary w-full">
+              Watch now
+            </button>
+          </div>
+        </div>
+      </section>
+
+      <section className="movies-layout page-container pb-20">
+        <h2 className="capitalize text-white mb-10 text-3xl font-bold">
+          Treding
+        </h2>
+        <div className="movie-list grid grid-cols-4 gap-10">
+          <div className="movie-card rounded-lg p-3 bg-slate-800 text-white">
+            <img
+              src="https://nld.mediacdn.vn/2019/4/25/3515432-endgamedek-15561710302491765206118.jpg"
+              alt=""
+              className="w-full h-[250px] object-cover rounded-lg mb-5"
+            />
+            <h3 className="text-xl font-bold mb-3">Onpiece</h3>
+            <div className="flex items-center justify-between text-sm opacity-50 mb-10">
+              <span>2023</span>
+              <span>9.0</span>
+            </div>
+            <button className="py-3 px-6 rounded-lg capitalize bg-primary w-full">
               Watch now
             </button>
           </div>
