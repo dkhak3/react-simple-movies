2b63f06f7099dc3351382d2559a0c6aad6a8e1b6
KHA  DUY <nhokkha345@gmail.com>
2023-05-02 22:33:16 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { NavLink } from "react-router-dom";
+import MovieList from "./components/movie/MovieList";
 
 function App() {
   return (
@@ -41,69 +41,21 @@ function App() {
         <h2 className="capitalize text-white mb-10 text-3xl font-bold">
           Now playing
         </h2>
-        <div className="movie-list grid grid-cols-4 gap-10">
-          <div className="movie-card rounded-lg p-3 bg-slate-800 text-white">
-            <img
-              src="https://nld.mediacdn.vn/2019/4/25/3515432-endgamedek-15561710302491765206118.jpg"
-              alt=""
-              className="w-full h-[250px] object-cover rounded-lg mb-5"
-            />
-            <h3 className="text-xl font-bold mb-3">Onpiece</h3>
-            <div className="flex items-center justify-between text-sm opacity-50 mb-10">
-              <span>2023</span>
-              <span>9.0</span>
-            </div>
-            <button className="py-3 px-6 rounded-lg capitalize bg-primary w-full">
-              Watch now
-            </button>
-          </div>
-        </div>
+        <MovieList></MovieList>
       </section>
 
       <section className="movies-layout page-container pb-20">
         <h2 className="capitalize text-white mb-10 text-3xl font-bold">
           Top rated
         </h2>
-        <div className="movie-list grid grid-cols-4 gap-10">
-          <div className="movie-card rounded-lg p-3 bg-slate-800 text-white">
-            <img
-              src="https://nld.mediacdn.vn/2019/4/25/3515432-endgamedek-15561710302491765206118.jpg"
-              alt=""
-              className="w-full h-[250px] object-cover rounded-lg mb-5"
-            />
-            <h3 className="text-xl font-bold mb-3">Onpiece</h3>
-            <div className="flex items-center justify-between text-sm opacity-50 mb-10">
-              <span>2023</span>
-              <span>9.0</span>
-            </div>
-            <button className="py-3 px-6 rounded-lg capitalize bg-primary w-full">
-              Watch now
-            </button>
-          </div>
-        </div>
+        <MovieList type="top_rated"></MovieList>
       </section>
 
       <section className="movies-layout page-container pb-20">
         <h2 className="capitalize text-white mb-10 text-3xl font-bold">
           Treding
         </h2>
-        <div className="movie-list grid grid-cols-4 gap-10">
-          <div className="movie-card rounded-lg p-3 bg-slate-800 text-white">
-            <img
-              src="https://nld.mediacdn.vn/2019/4/25/3515432-endgamedek-15561710302491765206118.jpg"
-              alt=""
-              className="w-full h-[250px] object-cover rounded-lg mb-5"
-            />
-            <h3 className="text-xl font-bold mb-3">Onpiece</h3>
-            <div className="flex items-center justify-between text-sm opacity-50 mb-10">
-              <span>2023</span>
-              <span>9.0</span>
-            </div>
-            <button className="py-3 px-6 rounded-lg capitalize bg-primary w-full">
-              Watch now
-            </button>
-          </div>
-        </div>
+        <MovieList type="popular"></MovieList>
       </section>
     </Fragment>
   );
